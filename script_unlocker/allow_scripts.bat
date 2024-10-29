3146e1a6380bc0a36deb5544e86086cafd0b1e1e
DiP1mb4 <168584544+DP1mb4@users.noreply.github.com>
2024-10-29 14:23:11 -0300

    Add files via upload

---
+++ script_unlocker/allow_scripts.bat
@@ -0,0 +1,19 @@
+@echo off
+color a
+title By DiP1mb4
+echo - By DiP1mb4
+echo.
+echo ======================
+echo = Aplicando Mudancas =
+echo ======================
+echo.
+powershell "Set-ExecutionPolicy ByPass -Scope CurrentUser"
+echo ======================
+echo =  Status - ByPass   =
+echo ======================
+echo.
+echo ======================
+echo = Mudancas Aplicadas =
+echo ======================
+echo.
+pause
---
+++ script_unlocker/default_script.bat
@@ -0,0 +1,19 @@
+@echo off
+color a
+title By DiP1mb4
+echo - By DiP1mb4
+echo.
+echo ======================
+echo = Aplicando Mudancas =
+echo ======================
+echo.
+powershell "Set-ExecutionPolicy Restricted -Scope CurrentUser"
+echo ===========================
+echo =   Status - Restircted   =
+echo ===========================
+echo.
+echo ======================
+echo = Mudancas Aplicadas =
+echo ======================
+echo.
+pause
